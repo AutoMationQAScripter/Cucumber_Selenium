b01d342d425d36d2c79ea988b237ac9938a1cd44
Deepak <paldeepak.itm@gmail.com>
2017-09-12 13:44:41 +0530

    Code updated

--- src/test/java/Cucumber_Selenium_Runner/Cucumber_Runner.java
+++ src/test/java/Cucumber_Selenium_Runner/Cucumber_Runner.java
@@ -1,7 +1,11 @@
 package Cucumber_Selenium_Runner;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.sun.jna.platform.unix.X11.Window;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -11,20 +15,32 @@ public class Cucumber_Runner {
 
 	WebDriver driver;
 	
-@Given("^Launch Browser and check Page$")
-public void launch_Browser_and_check_Page() throws Throwable {
-	driver = new FirefoxDriver();
-	driver.get("https://enterprise-demo.orangehrmlive.com/"); 
+	@Given("^Launch Browser and check Page$")
+	public void launch_Browser_and_check_Page() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "E:\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://Enterprise.Demo.Orangehrmlive.Com/");
+			  
+	}
+
+	@When("^Enter Username and Password$")
+	public void enter_Username_and_Password() throws Throwable {
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Deepak.Pal");
+		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("Deepak.Pal");
+		driver.findElement(By.xpath("//input[@name='Submit']")).click();
+
+	}
+
+	@Then("^Check invalid message$")
+	public void check_invalid_message() throws Throwable {
+		WebElement str = driver.findElement(By.xpath("//span[@id='spanMessage']"));
+		if(str.equals("Invalid credentials"))
+			System.out.println("Invalid credentials");
+		
+		driver.quit();
 	
-}
+	}
 
-@When("^Enter Username and Password$")
-public void enter_Username_and_Password() throws Throwable {
-    
-}
-
-@Then("^Check invalid message$")
-public void check_invalid_message() throws Throwable {
-    }
 
 }
